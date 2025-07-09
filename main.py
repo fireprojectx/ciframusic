40c58ac4016bf11a297b05ce62184df1745102b9
Wallace <167040540+fireprojectx@users.noreply.github.com>
2025-07-09 15:43:12 +0000

    melhorando estilo

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 def form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
+
 
 @app.post("/upload", response_class=HTMLResponse)
 async def upload(request: Request, file: UploadFile):
@@ -20,15 +22,43 @@ async def upload(request: Request, file: UploadFile):
     texto_extraido = extrair_texto_pdf(conteudo_pdf)
     resposta = formatar_com_gpt(texto_extraido)
 
+    linhas = separar_cifras_letra(resposta.get("cifra", ""))
+
     return templates.TemplateResponse("presentation.html", {
         "request": request,
         "titulo": resposta.get("titulo", "Sem título"),
         "autor": resposta.get("autor", "Desconhecido"),
-        "cifra": resposta.get("cifra", "")
-        # Não precisa passar "velocidade" aqui, pois será controlada na tela
+        "linhas": linhas
     })
+
 
 def extrair_texto_pdf(pdf_bytes):
     with fitz.open(stream=pdf_bytes, filetype="pdf") as doc:
         texto = "\n".join(page.get_text() for page in doc)
     return texto
+
+
+def separar_cifras_letra(cifra: str):
+    linhas_processadas = []
+    for linha in cifra.split('\n'):
+        cifra_linha = ""
+        letra_linha = ""
+        i = 0
+        while i < len(linha):
+            if linha[i] == '[':
+                j = linha.find(']', i)
+                if j != -1:
+                    acorde = linha[i:j+1]
+                    cifra_linha += acorde
+                    letra_linha += ' ' * (j + 1 - i)
+                    i = j + 1
+                else:
+                    cifra_linha += linha[i]
+                    letra_linha += ' '
+                    i += 1
+            else:
+                cifra_linha += ' '
+                letra_linha += linha[i]
+                i += 1
+        linhas_processadas.append({"cifra": cifra_linha, "letra": letra_linha})
+    return linhas_processadas
